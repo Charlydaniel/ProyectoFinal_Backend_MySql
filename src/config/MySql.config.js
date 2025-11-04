55eb78138accba0e32ab233f836d2bf29b517156
Charly Peralta <dan.bsas22@gmail.com>
2025-11-04 16:42:33 -0300

    04-11-2025-10

--- src/config/MySql.config.js
+++ src/config/MySql.config.js
@@ -1,27 +1,31 @@
+// db.js
 import mysql from 'mysql2/promise'
 import ENVIRONMENT from './environment.config.js'
 
 const pool = mysql.createPool({
-    host:ENVIRONMENT.MYSQ_HOST,
-    port:ENVIRONMENT.MYSQL_PORT,
-    user:ENVIRONMENT.MYSQL_USERNAME,
-    password:ENVIRONMENT.MYSQL_PASSWORD,
-    database:ENVIRONMENT.MYSQL_DATABASE,
-    waitForConnections: true,  
-    connectionLimit: 5,        
-    queueLimit: 0,           
-    enableKeepAlive: true,    
-    keepAliveInitialDelay: 0
-})
 
+  host: ENVIRONMENT.MYSQ_HOST,
+  port: ENVIRONMENT.MYSQL_PORT,
+  user: ENVIRONMENT.MYSQL_USERNAME,
+  password: ENVIRONMENT.MYSQL_PASSWORD,
+  database: ENVIRONMENT.MYSQL_DATABASE,
+  waitForConnections: true,  
+  connectionLimit: 5,        
+  queueLimit: 0,            
+  enableKeepAlive: true,    
+  keepAliveInitialDelay: 0
+
+});
 
 (async () => {
   try {
     const connection = await pool.getConnection();
-    console.log('Conexión exitosa a la base de datos');
-    connection.release();
+    console.log('✅ Conexión exitosa a la base de datos');
+    
+    connection.release(); 
   } catch (error) {
-    console.error('Error de conexión a la base de datos:', error);
+    console.error('❌ Error de conexión a la base de datos:', error);
   }
 })();
-export default pool
+
+export default pool;
